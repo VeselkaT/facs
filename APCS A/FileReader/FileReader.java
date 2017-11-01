bc53d34cf567b24120931411a86e641aea3239b9
igloowhite <dave.sartory@gmail.com>
2017-11-01 16:29:21 -0400

    Cleanup

--- APCS A/FileReader/FileReader.java
+++ APCS A/FileReader/FileReader.java
@@ -1,20 +1,7 @@
-package filereader;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- *
- * @author dsartory
- */
 public class FileReader {
 
     /**
@@ -23,11 +10,11 @@ public class FileReader {
      */
     public static void main(String[] args) throws FileNotFoundException {
         try (
-            Scanner scanner = new Scanner(new File("src/filereader/data.txt"))) {
-            scanner.useDelimiter(",");
+            Scanner input = new Scanner(new File("src/filereader/data.txt"))) {
+            input.useDelimiter(",");
             
             while(scanner.hasNext()){
-                System.out.print(scanner.next()+"|");
+                System.out.print(input.next()+"|");
             }
         }
     }
